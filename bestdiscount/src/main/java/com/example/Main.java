f0dfd6c705ae7acfc9e65d149c2e6d25dd80f91e
MaxenV <maxen2001@gmail.com>
2025-05-11 22:39:12 +0200

    Modify build config
    Change path to .json files to work when build from terminal

--- bestdiscount/src/main/java/com/example/Main.java
+++ bestdiscount/src/main/java/com/example/Main.java
@@ -7,8 +7,8 @@ import com.example.classes.PaymentOptimizer;
 
 public class Main {
     public static void main(String[] args) {
-        Orders clientOrders = new Orders("bestdiscount/src/main/resources/data/orders.json");
-        ClientWallet ClientWallet = new ClientWallet("bestdiscount/src/main/resources/data/paymentmethods.json");
+        Orders clientOrders = new Orders("src/main/resources/data/orders.json");
+        ClientWallet ClientWallet = new ClientWallet("src/main/resources/data/paymentmethods.json");
 
         for (Order order : clientOrders.getOrders()) {
             PaymentOptimizer optimal = new PaymentOptimizer(ClientWallet, order);
